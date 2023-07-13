f775da2559684c579964c6ce396976d806823ee5
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-07-13 12:08:22 +0300

    completed AboutMe component

---
+++ components/AboutMe.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const AboutMe = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl items-center justify-evenly mx-auto px-10"
+    >
+      <div className="space-y-10 px-0 md:px-10">
+        <h3 className="text-3xl font-semibold text-gray-200">Özgenur Özkara</h3>
+        <p className="text-gray-500">
+          "I'm a Front End Developer with over 1+ year of experience in
+          developing websites and software products. Throughout my experience, I
+          have worked on projects using NextJS, Redux, and Tailwind CSS,
+          improving my skills and broadening my knowledge. While my primary
+          focus has been on development, I have discovered a passion for design
+          along the way. As web projects progress, I notice that my perspective
+          on aesthetics and user experience improves. I even share some of my
+          designs on Dribbble."
+        </p>
+      </div>
+      <h2 className="absolute top-[100px] text-gray-200 text-3xl tracking-[5px]">
+        About Me
+        <div className="h-px w-14 bg-gray-400"></div>
+      </h2>
+      <motion.img
+        initial={{ x: 200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        viewport={{ once: true }}
+        src="https://media.licdn.com/dms/image/D4D03AQFCbGuqUohg5w/profile-displayphoto-shrink_800_800/0/1671976917166?e=2147483647&v=beta&t=jqC4evYtmj4UJRpJosBclfifHwWjCLWKzyyDoiWFkuE"
+        alt="profile"
+        className="w-48 h-48 rounded-full md:h-[300px] md:w-72 -mb-20 md:mb-0"
+      />
+    </motion.div>
+  );
+};
+
+export default AboutMe;
--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
   });
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center text-center space-y-8 overflow-hidden">
+    <div className="h-full flex flex-col items-center justify-center text-center space-y-8 overflow-hidden">
       <Circle />
       <Image
         src="https://media.licdn.com/dms/image/D4D03AQFCbGuqUohg5w/profile-displayphoto-shrink_800_800/0/1671976917166?e=2147483647&v=beta&t=jqC4evYtmj4UJRpJosBclfifHwWjCLWKzyyDoiWFkuE"
@@ -28,23 +28,21 @@ const HeroSection = () => {
         className="rounded-full"
       />
       <div className="z-20">
-        {" "}
         <h2 className="text-sm uppercase text-gray-400 pb-3 tracking-[12px]">
           {title}
-          <Cursor cursorColor="#ca3131" />
+          <Cursor cursorColor="#a3e635" />
         </h2>
-        <h1 className="text-4xl text-indigo-900 lg:text-6xl font-semibold px-8">
+        <h1 className="text-4xl text-[#a3e635] lg:text-6xl font-semibold px-8">
           <span>{text}</span>
-          <Cursor cursorColor="#ca3131" />
+          <Cursor cursorColor="#a3e635" />
         </h1>
         <div className="pt-6 space-x-2">
-          {" "}
-          <Link href="/">
+          <Link href="#aboutme">
             <button className="heroButton text-[#626974] hover:text-gray-300">
               About
             </button>
           </Link>
-          <Link href="/">
+          <Link href="https://uiverse.io/profile/ozgeozkaraa01" target="_blank">
             <button className="heroButton text-[#626974] hover:text-gray-300">
               uiverse
             </button>
