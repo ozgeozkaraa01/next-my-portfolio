4cc6d1f1f5473cb696a574f9013ad1e90e953fb3
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-07-13 13:23:50 +0300

    the pointer-events property has been added to the stars component

--- components/Stars.jsx
+++ components/Stars.jsx
@@ -4,14 +4,15 @@ import { motion } from "framer-motion";
 const Stars = () => {
   return (
     <motion.div
-      className="absolute top-0 left-0 h-full w-full overflow-hidden"
+      className="fixed top-0 left-0 h-full w-full overflow-hidden"
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 3, repeat: Infinity, repeatType: "mirror" }}
+      style={{ pointerEvents: "none" }}
     >
       <svg
-        className="absolute h-[20px] w-[20px] text-white"
+        className="absolute h-[10px] w-[10px] text-white"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="none"
