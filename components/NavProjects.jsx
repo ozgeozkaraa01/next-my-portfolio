c1faf3dd69545086aa2c5b948ad0f97a41933a38
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-08-04 12:44:28 +0300

    feat: Add dropdown menu to HiMenuAlt1 button

--- components/NavProjects.jsx
+++ components/NavProjects.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { HiMenuAlt1 } from "react-icons/hi";
-import { FiSettings } from "react-icons/fi";
+import { FiSettings, FiGithub, FiTwitter } from "react-icons/fi";
 import Link from "next/link";
 
 const NavProjects = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen2, setIsMenuOpen2] = useState(false);
 
   const handleMenuToggle = () => {
     setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const handleMenuToggle2 = () => {
+    setIsMenuOpen2((prevState) => !prevState);
   };
 
   return (
@@ -15,7 +20,10 @@ const NavProjects = () => {
       <div className="mx-auto px-2">
         <div className="relative flex items-center justify-between h-16">
           <div className="relative inline-block text-left">
-            <button className="group relative inline-flex items-center px-3 py-2 bg-gray-50 hover:bg-gray-100 hover:text-gray-700 text-gray-400 rounded-lg text-base font-medium default-transition default-focus">
+            <button
+              onClick={handleMenuToggle2}
+              className="group relative inline-flex items-center px-3 py-2 bg-gray-50 hover:bg-gray-100 hover:text-gray-700 text-gray-400 rounded-lg text-base font-medium default-transition default-focus"
+            >
               <HiMenuAlt1 />
             </button>
           </div>
@@ -34,7 +42,7 @@ const NavProjects = () => {
               <div className="py-2">
                 <Link
                   href="/"
-                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -56,7 +64,7 @@ const NavProjects = () => {
                 <hr className="mt-2 pb-2 border-gray-200 " />
                 <Link
                   href="/"
-                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +85,7 @@ const NavProjects = () => {
                 </Link>
                 <Link
                   href="/"
-                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -96,6 +104,123 @@ const NavProjects = () => {
 
                   <span>Dark Theme</span>
                   <span className="flex-1"></span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
+        {isMenuOpen2 && (
+          <div className="transform scale-100 opacity-100">
+            <div className="absolute w-[calc(100vw-1rem)] sm:w-56 mt-2 bg-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-sm border border-gray-100 rounded-md shadow-lg divide-y divide-gray-100 focus:outline-none origin-top-left left-0">
+              <div className="py-2">
+                {" "}
+                <Link
+                  href="/"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                    />
+                  </svg>
+                  <span>Home</span>
+                  <span className="flex-1"></span>
+                </Link>
+                <Link
+                  href="/"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                    />
+                  </svg>
+                  <span>Blog</span>
+                  <span className="flex-1"></span>
+                </Link>
+                <Link
+                  href="/"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6"
+                    />
+                  </svg>
+                  <span>Projects</span>
+                  <span className="flex-1"></span>
+                </Link>
+                <hr className="mt-2 pb-2 border-gray-200 " />
+                <Link
+                  href="/"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
+                >
+                  <FiTwitter className="w-4 h-4" />
+                  <span>Twitter</span>
+                  <span className="flex-1"></span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                  </svg>
+                </Link>
+                <Link
+                  href="/"
+                  className="flex items-center space-x-2 px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default:transition text-gray-500 hover:text-gray-700 hover:bg-slate-200"
+                >
+                  <FiGithub className="w-4 h-4" />
+                  <span>GitHub</span>
+                  <span className="flex-1"></span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                  </svg>
                 </Link>
               </div>
             </div>
