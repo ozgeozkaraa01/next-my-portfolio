d0b08a4c0aa47e56cd0247ff00d54cd294b3426a
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-08-23 11:37:50 +0300

    feat: added dynamic page titles and SEO with next-seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { NextSeo } from "next-seo";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
+  const getPageTitle = (router) => {
+    switch (router.pathname) {
+      case "/bookmarks":
+        return "Özgenur Özkara - Bookmarks";
+      case "/projects":
+        return "Özgenur Özkara - Projects";
+      default:
+        return "Özgenur Özkara";
+    }
+  };
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <NextSeo
+        title={getPageTitle(router)}
+        description="Personal portfolio website of Özgenur Özkara"
+      />
       <Head>
-        <title>Özgenur Özkara</title>
-        <meta
-          name="description"
-          content="Personal portfolio website of Özgenur Özkara"
-        />
         <link
           rel="icon"
           href="https://media.licdn.com/dms/image/D4D03AQFCbGuqUohg5w/profile-displayphoto-shrink_800_800/0/1671976917166?e=2147483647&v=beta&t=jqC4evYtmj4UJRpJosBclfifHwWjCLWKzyyDoiWFkuE"
