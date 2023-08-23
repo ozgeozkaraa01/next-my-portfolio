fefc24db3be96426c9532eb26a87d1344e8adb0d
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-08-23 11:24:36 +0300

    feat: added global <Head> content in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <Head>
+        <title>Özgenur Özkara</title>
+        <meta
+          name="description"
+          content="Personal portfolio website of Özgenur Özkara"
+        />
+        <link
+          rel="icon"
+          href="https://media.licdn.com/dms/image/D4D03AQFCbGuqUohg5w/profile-displayphoto-shrink_800_800/0/1671976917166?e=2147483647&v=beta&t=jqC4evYtmj4UJRpJosBclfifHwWjCLWKzyyDoiWFkuE"
+        />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import AboutMe from "../components/AboutMe";
@@ -6,17 +5,6 @@ import AboutMe from "../components/AboutMe";
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-[#F6FFDE] dark:bg-gray-900 overflow-y-scroll">
-      <Head>
-        <title>Özgenur Özkara</title>
-        <meta
-          name="description"
-          content="Personal portfolio website of Özgenur Özkara"
-        />
-        <link
-          rel="icon"
-          href="https://media.licdn.com/dms/image/D4D03AQFCbGuqUohg5w/profile-displayphoto-shrink_800_800/0/1671976917166?e=2147483647&v=beta&t=jqC4evYtmj4UJRpJosBclfifHwWjCLWKzyyDoiWFkuE"
-        />
-      </Head>
       <Header />
       <HeroSection />
       <section id="aboutme">
